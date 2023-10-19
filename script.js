99c276a8f81d92bdbdb0b4c918c6975e28f43f78
Austin Vichich <avichich4@gmail.com>
2023-10-18 20:37:32 -0600

    Added headers, paragraphs, lists, and information

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Software Engineering Student";
 
 document.querySelector("body").appendChild(h2);
 
